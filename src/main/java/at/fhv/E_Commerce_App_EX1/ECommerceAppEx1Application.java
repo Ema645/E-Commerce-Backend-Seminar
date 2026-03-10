c8ed5bf03453fb1a3f49e0c88343b1d4c6d0dab1
Emanuel Peter <157378848+Ema645@users.noreply.github.com>
2026-03-10 14:59:30 +0100

    initialize E-Commerce application with Gradle setup and basic structure

---
+++ src/main/java/at/fhv/E_Commerce_App_EX1/ECommerceAppEx1Application.java
@@ -0,0 +1,13 @@
+package at.fhv.E_Commerce_App_EX1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAppEx1Application.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhv/E_Commerce_App_EX1/ECommerceAppEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.E_Commerce_App_EX1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
